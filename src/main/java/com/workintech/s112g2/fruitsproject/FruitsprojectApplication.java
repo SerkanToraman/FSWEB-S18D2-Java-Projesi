116c95b845f8de64718fe7091025b99427e801b0
serkantoraman <serkan@github.com>
2023-08-31 14:01:23 +0300

    Init Commit

---
+++ src/main/java/com/workintech/s112g2/fruitsproject/FruitsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.s112g2.fruitsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitsprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/s112g2/fruitsproject/FruitsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.s112g2.fruitsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
